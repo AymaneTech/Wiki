18e3a134bba33dd12ba8463d88619fd8ebbf6abf
aymane <elmainiaymane03@gmail.com>
2024-01-08 20:39:19 +0100

    i add wikis

--- App/Models/Services/WikiService.php
+++ App/Models/Services/WikiService.php
@@ -1,7 +1,25 @@
 <?php
 
 namespace App\Models\Services;
+use App\Models\entities\CategoryEntity;
+use App\Models\entities\UserEntity;
+use App\Models\entities\WikiEntity;
+use App\Models\repositories\WikiRepository;
+
 class WikiService
 {
+    private $wikiRepository;
+    public function __construct(){
+        $this->wikiRepository = new WikiRepository();
+    }
+    public function saveWiki($wiki){
+        extract($wiki);
+        $categoryEntity = new CategoryEntity();
+        $authorEntity = new UserEntity();
+        $categoryEntity->__set("categoryId", $category);
+        $authorEntity->__set("userId", $authorId);
+        $wikiEntity = new WikiEntity($wikiTitle, $wikiDescription, $wikiContent, $image, $categoryEntity, $authorEntity);
+        $this->wikiRepository->saveWiki($wikiEntity);
+    }
 
 }
---
+++ App/Models/repositories/WikiRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\repositories;
+
+use App\Models\entities\WikiEntity;
+
+class WikiRepository extends \App\Core\Model
+{
+    public function __construct()
+    {
+        parent::__construct("wiki");
+    }
+
+    public function saveWiki(WikiEntity $wikiEntity)
+    {
+//        dd($wikiEntity->__get("author"));
+        $data = ["wikiTitle" => $wikiEntity->__get("wikiTitle"),
+            "wikiDescription" => $wikiEntity->__get("wikiDescription"),
+            "wikiContent" => $wikiEntity->__get("wikiContent"),
+            "wikiImage" => $wikiEntity->__get("wikiImage"),
+            "categoryId" => $wikiEntity->__get("category")->__get("categoryId"),
+            "authorId" => $wikiEntity->__get("author")->__get("userId")
+        ];
+        $this->save($data);
+    }
+}
