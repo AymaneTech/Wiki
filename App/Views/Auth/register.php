f3453605f36a466291e8aa56c90d3142c2748942
aymane <elmainiaymane03@gmail.com>
2024-01-12 04:18:53 +0100

    update the lengh of username

--- App/Views/Auth/register.php
+++ App/Views/Auth/register.php
@@ -23,7 +23,7 @@
                         <input required
                                id="username" name="username" type="text"
                                class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                               placeholder="Email address" minlength="10"/>
+                               placeholder="Email address" minlength="5"/>
                         <label for="username"
                                class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Username
                         </label>
